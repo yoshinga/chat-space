66ae0204f0c8c128f943847ab740292543d68552
yoshinga <y.tomo41@icloud.com>
2019-10-28 19:56:44 +0900

    変更しました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,34 @@
+
 class MessagesController < ApplicationController
+  before_action :set_group
+  @inport = 'message';
+
   def index
+    # @users = Users.find(params[:user])
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
-  def new 
+  
+  def create
+    @message = @group.messages.new(message_params)
+    if  @message.save
+      redirect_to group_messages_path(@group),notice: 'メッセージ送信されたよ^ ^'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージは入れなよ'
+      render :index 
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'messages#index'
+  devise_for :users
+  root to: 'groups#index'
 
   resources :messages
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update, :show] do
+    resources :messages, only: [:index, :create, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
