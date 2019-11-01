b8113fa783fcb3bb15067ba48679cb88022fec92
yoshinga <y.tomo41@icloud.com>
2019-11-01 15:27:42 +0900

    ajaxの実装

---
+++ app/assets/javascripts/create.js
@@ -0,0 +1,43 @@
+$(function() {
+
+  function buildMessage(message){
+    let html = `<div class="message">
+                  <div class="message__upper-info__talker">
+                    ${ message.user_name }
+                      <div class="message__upper-info__date">
+                        ${message.time}            
+                      </div>
+                  </div>
+                  <div class="message__text">
+                    <p class="lower-message__content">
+                      ${message.content}                     
+                    </p>
+                  </div>
+                </div>`
+    return html;
+  }
+  $('#new_message').on("submit", function(e) {
+    e.preventDefault();
+    let formData = new FormData(this)
+    let url = $(this).attr('action');
+    $.ajax({
+      type:'POST',
+      url: url,
+      data: formData,
+      datatype:  'json',
+      processData: false,
+      contentType: false
+    })
+    .fail(function(){
+      alert('メッセージが送れませんでした');
+      $('.form__submit').prop('disabled', false);
+    })
+    .done(function(data){
+      let html = buildMessage(data);
+      $(".messages").append(html);
+      $(".form__message").val('');
+      $('.form__submit').prop('disabled', false);
+      $(".messages").animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast')
+    })
+  });
+});
