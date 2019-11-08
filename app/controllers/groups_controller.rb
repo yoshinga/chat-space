4c45af2a527a4b680f0e784feb65b1bec2b0f74a
yoshinga <y.tomo41@icloud.com>
2019-11-08 15:04:11 +0900

    update groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,7 +24,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを編集しました'
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
     end
